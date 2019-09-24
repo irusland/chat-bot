1c48d7ef0e4e9a50006faeb28d7c177bf8f434b5
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2019-09-24 09:14:22 +0500

    Class separation

---
+++ project/src/Bot.java
@@ -0,0 +1,109 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Bot {
+
+    private String name;
+    private BufferedReader reader;
+    private PrintStream writer;
+    public Bot(InputStream in, PrintStream out)
+    {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = System.out;
+    }
+
+    private Map<String, String[]> questionVariations = new HashMap<>() {{
+        put("name", new String[] {"Как тебя зовут?", "Как имя твоё?", "Какое у тебя имя?"});
+        put("nickname", new String[] {"Как тебя называют?"});
+        put("surname", new String[] {"Какая у тебя фамилия?"});
+        put("middleName", new String[] {"Как твое отчество?"});
+        put("mood", new String[] {"Как дела?", "Как настрой?", "Как настроение?"});
+    }};
+
+    private Map<String, ArrayList<String>> knowledge = new HashMap<>() {};
+
+    public void Start() throws IOException { // Почему надо писать throws?
+        int questionPointer = 0;
+        var refused = false;
+        writer.println("Сейчас я буду задавать личные вопросы, постарайся отвечать честно!");
+        while (questionPointer < questionVariations.keySet().toArray().length) {
+            var questionType = GetQuestion(questionPointer);
+            Ask(questionType);
+
+            String answer = reader.readLine();
+            if (!IsValid(answer, questionType)) {
+                refused = true;
+                while (refused)
+                {
+                    Refuse();
+                    Ask(questionType);
+                    answer = reader.readLine();
+                    if (UserCanceled(answer))
+                        break;
+                    if (IsValid(answer, questionType))
+                        refused = false;
+                }
+            }
+            if (refused) {
+                writer.println("Понятно, что ты не хочешь отвечать на вопрос");
+                continue;
+            }
+            Approve();
+            questionPointer++;
+            if (!knowledge.containsKey(questionType))
+                knowledge.put(questionType, new ArrayList<>());
+            knowledge.get(questionType).add(answer);
+//            writer.println(knowledge);
+        }
+        writer.println("Вот интересная информация о тебе:");
+        var info = ProcessData(knowledge);
+        writer.println(knowledge);
+        writer.println(info);
+    }
+
+    private String ProcessData(Map<String, ArrayList<String>> knowledge) {
+        return null;
+    }
+
+    private boolean UserCanceled(String answer) {
+        return false;
+    }
+
+    private void Refuse() {
+        writer.println(GetRandomItem(botRefuse));
+    }
+
+    private String[] botApprove = new String[] {"Окей!", "Ответ записан!", "Хорошо", "Приятно слышать", "Как здорово!"};
+    private String[] botRefuse = new String[] {"Не, так не пойдет!", "Не знаю такго!", "Попробую еще раз", "Я не понял тебя"};
+
+    private void Approve() {
+        writer.println(GetRandomItem(botApprove));
+    }
+
+    private boolean IsValid(String answer, String type) {
+        return true; //TODO answer validation
+    }
+
+    private String GetRandomItem(String[] questions) {
+        Random r = new Random();
+        var max = questions.length;
+        var rnd = r.nextInt((max));
+        return questions[rnd];
+    }
+
+    private void Ask(String type) {
+        var questions = questionVariations.get(type);
+        var question = GetRandomItem(questions);
+        writer.println(question);
+    }
+
+    private String GetQuestion(int pointer) {
+        var list = questionVariations.keySet().toArray();
+        return Array.get(list, pointer).toString();
+    }
+}
+
