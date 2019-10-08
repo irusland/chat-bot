8dc8dd462ac200e53c8978d6c6da5fdb268060e8
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2019-10-08 13:54:21 +0500

    Async games

--- project/src/Bot.java
+++ project/src/Bot.java
@@ -4,14 +4,18 @@ import game.tictactoe.TicTacToe;
 import org.json.simple.parser.ParseException;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 
 public class Bot {
     private BufferedReader reader;
     private PrintStream writer;
+    private ArrayList<Game> processes;
 
     public Bot(InputStream in, PrintStream out) throws IOException, ParseException {
         reader = new BufferedReader(new InputStreamReader(System.in));
         writer = System.out;
+        processes = new ArrayList<>();
     }
 
     public void Start() throws Exception {
@@ -22,22 +26,53 @@ public class Bot {
             var choise = reader.readLine();
             switch (choise) {
                 case "/xo":
-                    Play(new TicTacToe(3));
+                    Play(TicTacToe.class);
                     break;
                 case "/ship":
-                    Play(new ShipWars());
+                    Play(ShipWars.class);
                     break;
             }
         }
     }
 
-    private void Play(Game game) throws Exception {
+    private void Play(Class cls) throws Exception {
+        System.out.println("Playing " + cls.toString());
+        var classFound = false;
+        Game game = null;
+        for (var proc : processes) {
+            if (proc.getClass().isAssignableFrom(cls)) {
+                classFound = true;
+                game = proc;
+                break;
+            }
+        }
+        if (classFound) {
+            Continue(game);
+        } else {
+            game = Restore(cls);
+            processes.add(game);
+            Continue(game);
+        }
+    }
+
+    private Game Restore(Class cls) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        Game game = (Game)cls.getDeclaredConstructor().newInstance();
         writer.println(game.Start());
+        return game;
+    }
+
+    private void Continue(Game game) throws Exception {
         while (!game.IsFinished()) {
             var query = reader.readLine();
+            if (query.equals("/pause")) {
+                writer.println("Game paused");
+                break;
+            }
             var response = game.Request(query);
             writer.println(response);
         }
     }
+
+
 }
 
